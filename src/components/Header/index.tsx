11e99a069ff464b2a10566595ec19e94c676f21c
allanzaparoli <allanzaparoli@gmail.com>
2023-08-02 18:11:36 -0300

    criando header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,20 @@
 import { HeaderContainer } from "./styles";
+import { Timer, Scroll } from 'phosphor-react';
+
+import logo from '../../assets/logo.svg';
+import { NavLink } from "react-router-dom";
 
 export function Header() {
   return (
     <HeaderContainer>
-      <span>Logo</span>
+      <img src={logo} alt="" />
       <nav>
-        <a href="">
-          timer
-        </a>
-        <a href="">
-          history
-        </a>
+        <NavLink to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
