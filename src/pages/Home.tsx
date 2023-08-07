4e8dd5c93332547f8725ff6ad745ed95a796f58a
allanzaparoli <allanzaparoli@gmail.com>
2023-08-07 15:49:07 -0300

    criando a home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,30 @@
+import { Play } from "phosphor-react";
+
 export function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <form action="">
+        <label htmlFor="">Vou Trabalhar em</label>
+        <input id="task" />
+
+        <label htmlFor="minutosAmount">durante</label>
+        <input type="number" id="minutesAmount" />
+
+        <span>minutos.</span>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </div>
   )
 }
