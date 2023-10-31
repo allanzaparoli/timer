7bc6f3b9168f008e9627efc3e09f1c0c0cac146f
allanzaparoli <allanzaparoli@gmail.com>
2023-10-31 15:24:27 -0300

    atualizando mudanças

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -16,18 +16,6 @@ export const HomeContainer = styled.main`
   }
 `
 
-export const FormContainer = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 0.5rem;
-  color: ${(props) => props.theme['gray-100']};
-  font-size: 1.125rem;
-  font-weight: bold;
-  flex-wrap: wrap;
-`
-
 const BaseInput = styled.input`
   background: transparent;
   height: 2.5rem;
@@ -47,19 +35,6 @@ const BaseInput = styled.input`
 &::placeholder {
   color: ${(props) => props.theme['gray-500']};
 }
-`
-
-export const TaskInput = styled(BaseInput)`
-  flex: 1;
-
-  &::-webkit-calendar-picker-indicator {
-    display: none !important;
-  }
-`
-
-export const MinutesAmountInput = styled(BaseInput)`
-  width: 4rem;
-  align-items: center;
 `
 
 export const CountdownContainer = styled.div`
